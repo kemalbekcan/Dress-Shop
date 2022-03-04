8fe7ad1de733e40f06f50c86629e3b6f805b4358
kemalbekcan <kemalbekcan@gmail.com>
2022-03-04 23:35:05 +0300

    Dress E-commerce Application

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,34 @@
+import ModelImage from "../images/Model.svg"
 function Main() {
     return (
         <section className="shop">
             <h1 className="shop__title">Ladies</h1>
             <div className="shop-container">
                 <div className="shop-container-left">
+                <img src={ModelImage} />
                 </div>
                 <div className="shop-container-right">
+                <p>Floral Print Notched Neckline Dress Without Belt Notched Neckline</p>
+                <span className="shop-container-right__price">17,00 &euro;</span>
+                <span className="shop-container-right__size">Size</span>
+                <div className="shop-container-right-size">
+                    <div className="shop-container-right-size-circle">S</div>
+                    <div className="shop-container-right-size-circle">M</div>
+                    <div className="shop-container-right-size-circle">L</div>
+                </div>
+                <span className="shop-container-right__color">Color</span>
+                <div className="shop-container-right-size">
+                    <div className="shop-container-right-size-circle__brown"></div>
+                    <div className="shop-container-right-size-circle__black"></div>
+                    <div className="shop-container-right-size-circle__green"></div>
+                </div>
+                <span className="shop-container-right__quantity">Quantity</span>
+                <div className="shop-container-right-quantity">
+                    <button>-</button>
+                    <span>1</span>
+                    <button>+</button>
+                </div>
+                <button className="primary">ADD TO BASKET</button>
                 </div>
             </div>
         </section>
---
+++ src/utils/products.js
@@ -0,0 +1,11 @@
+const dress = {
+    brown: {
+        price: 17.00
+    },
+    black: {
+        price: 20.00
+    },
+    green: {
+        price: 25.00
+    }
+}
